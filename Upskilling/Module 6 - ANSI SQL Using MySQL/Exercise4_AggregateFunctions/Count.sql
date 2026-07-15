02abea83cfd2f2da2e6691fd32db7a5b13aa5ebc
saibabu04 <saibabus625@gmail.com>
2026-07-15 21:29:46 +0530

    Add SQL script to count various database records

---
+++ Upskilling/Module 6 - ANSI SQL Using MySQL/Exercise4_AggregateFunctions/Count.sql
@@ -0,0 +1,26 @@
+USE CommunityEventDB;
+
+
+
+SELECT COUNT(*) AS TotalUsers
+FROM Users;
+
+
+
+SELECT COUNT(*) AS TotalEvents
+FROM Events;
+
+
+
+SELECT COUNT(*) AS TotalSessions
+FROM Sessions;
+
+
+
+SELECT COUNT(*) AS TotalRegistrations
+FROM Registrations;
+
+
+
+SELECT COUNT(*) AS TotalFeedback
+FROM Feedback;
