16014a64506a4f039805126adf10bfd67f730f6c
saibabu04 <saibabus625@gmail.com>
2026-07-19 18:52:15 +0530

    Add LeapYear class to determine leap years

---
+++ Deepskilling/Module-4-NUnit-and-Moq/7-LeapYearCalculator/LeapYearLib/LeapYear.cs
@@ -0,0 +1,16 @@
+namespace LeapYearLib
+{
+    public class LeapYear
+    {
+        public bool IsLeapYear(int year)
+        {
+            if (year % 400 == 0)
+                return true;
+
+            if (year % 100 == 0)
+                return false;
+
+            return year % 4 == 0;
+        }
+    }
+}
