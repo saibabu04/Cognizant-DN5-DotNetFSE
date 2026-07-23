a7df63097fa5342f303fba9cfb9c416803ae2c39
saibabu04 <saibabus625@gmail.com>
2026-07-23 19:04:23 +0530

    Initialize KafkaWebApi with basic setup

---
+++ Deepskilling/Module-6-ASPNETCore-WebAPI/WebApi-Handson-6/KafkaWebApi/Program.cs
@@ -0,0 +1,29 @@
+using KafkaWebApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<KafkaProducer>();
+
+builder.Services.AddHostedService<KafkaConsumer>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
